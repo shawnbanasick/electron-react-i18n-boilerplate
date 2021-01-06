fef03a9f44c981665f740031080b5bc2c6eacc9c
Shawn <banasick@mail.kobe-c.ac.jp>
2021-01-06 23:41:25 +0900

    fixed missing translation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>{t("Help")}</h1>
+          <h1>{t("Language")}</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
